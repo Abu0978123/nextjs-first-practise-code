44e690860d1ae9901eaab2b8c2ca36a1d6d3ceae
Abu Bakar <abubakar0978123@gmail.com>
2022-10-10 00:13:38 +0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -3,5 +3,5 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
+ 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
+            href="https://github.com/abu0978123"
             className={styles.card}
           >
             <h2>Examples &rarr;</h2>
